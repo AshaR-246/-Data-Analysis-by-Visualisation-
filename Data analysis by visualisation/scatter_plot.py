0ee96149ce6152836b2d67faf0d2f33995b23e70
AshaR-246 <62698273+AshaR-246@users.noreply.github.com>
2021-05-10 17:13:28 +0530

    Update scatter_plot.py

--- Data analysis by visualisation/scatter_plot.py
+++ Data analysis by visualisation/scatter_plot.py
@@ -4,7 +4,7 @@ import plotly.express as px
 df=pd.read_csv("data.csv")
 print(df.groupby("level")["attempt"].mean())
 as_index=False
-fig = px.scatter(df, x="level", y="attempt",
+fig = px.scatter(df, x="student_id", y="level",
 	         color="attempt",size="attempt")
 fig.show()
 
